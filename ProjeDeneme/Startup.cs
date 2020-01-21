b61bc89b6290c9f8ab15e3adfcbb01c3d07d98d2
mustafaalby <albymusty9561@gmail.com>
2020-01-21 15:04:36 +0300

    staticFiles are added

--- ProjeDeneme/Startup.cs
+++ ProjeDeneme/Startup.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 
 namespace ProjeDeneme
 {
@@ -13,6 +15,7 @@ namespace ProjeDeneme
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Latest);
@@ -21,17 +24,25 @@ namespace ProjeDeneme
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseStaticFiles();//wwwroot
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(Directory.GetCurrentDirectory(), "node_modules")
+                ),
+                RequestPath = "/modules"
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
             //app.UseMvcWithDefaultRoute();
-            app.UseMvc(routes=>
+            app.UseMvc(routes =>
             {
                 routes.MapRoute(
-                    name:"default",
-                    template:"{controller=Home}/{action=index}/{id?}"
+                    name: "default",
+                    template: "{controller=Home}/{action=index}/{id?}"
                 );
             }
             );
